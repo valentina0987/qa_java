1aae01d0de1658de3dc203dae21b594ef8bd8208
coss <cossx@yahoo.com>
2022-03-19 20:22:19 +0400

    added changes

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,25 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CatTest {
+
+    @Test
+    public void shouldBeRightSound(){
+        Feline feline = new Feline();
+        Cat cat = new Cat(feline);
+        String expectedSound = "Мяу";
+        String actualSound = cat.getSound();
+        Assert.assertEquals(expectedSound, actualSound);
+    }
+
+    @Test
+    public void shouldBePredatorsFood() throws Exception {
+        Feline feline = new Feline();
+        Cat cat = new Cat(feline);
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        List<String> actualFood = cat.getFood();
+        Assert.assertEquals(expectedFood, actualFood);
+    }
+}
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,5 +1,3 @@
-import com.example.Lion;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
